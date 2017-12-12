e75e49890f0c64424c50c733c656b2b55dca4a81
Sebastian Strus <sebastian.strus1@gmail.com>
2017-12-12 14:35:49 +0100

    first commit

---
+++ App.java
@@ -0,0 +1,8 @@
+public class App {
+
+    public static void main(String[] args) {
+        
+        Animal dog = new Dog();
+        dog.sayHello();
+    }
+}
---
+++ Dog.java
@@ -0,0 +1,13 @@
+public class Dog implements Animal {
+
+    public void sayHi() {
+         System.out.println("Woof!");    
+    };
+
+    @Override
+    public void sayHello() {
+         System.out.println("Woof woof!");    
+    };
+    
+}
+
